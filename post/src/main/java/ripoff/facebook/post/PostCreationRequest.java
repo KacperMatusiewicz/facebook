2d8462795eb471320464ac0d16137e2212f162f8
KacperMatusiewicz <kacper.piotr.matusiewicz@gmail.com>
2022-03-15 18:11:17 +0100

    FB-14 amqp

--- post/src/main/java/ripoff/facebook/post/PostCreationRequest.java
+++ post/src/main/java/ripoff/facebook/post/PostCreationRequest.java
@@ -3,6 +3,9 @@ package ripoff.facebook.post;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+import java.util.Set;
+
 @Data
 @Setter
 @NoArgsConstructor
@@ -12,4 +15,6 @@ public class PostCreationRequest {
 
     private Long userId;
     private String content;
+    private Long visibilityGroupId;
+    private Set<Long> visibilityUsersId;
 }
--- post/src/main/java/ripoff/facebook/post/PostService.java
+++ post/src/main/java/ripoff/facebook/post/PostService.java
@@ -5,16 +5,19 @@ import org.springframework.stereotype.Service;
 import ripoff.facebook.post.exceptions.BadPostDataException;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
 public class PostService {
 
     private PostRepository repository;
+    private RelationService relationService;
     private TimeService timeService;
     private PostDataValidationService postDataValidationService;
 
     public void createPost(PostCreationRequest request){
+        Set<Long> visibilityUserId;
 
         if(!postDataValidationService.validatePost(request)){
             throw new BadPostDataException("Post data is not correct.");
@@ -26,8 +29,15 @@ public class PostService {
                 .creationDate(timeService.getCurrentDateTime())
                 .attachmentPath(null)
                 .build();
-
         repository.save(post);
+        if (request.getVisibilityGroupId() != null) {
+            visibilityUserId = relationService.getGroupIds(request.getVisibilityGroupId());
+        } else {
+            visibilityUserId = request.getVisibilityUsersId();
+        }
+        //exception jesli obydwa sa nullem
+        //dodac do kolejki
+
     }
 
     public List<Post> getAllPostsByUser(Long userId) {
