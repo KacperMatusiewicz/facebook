f9f47e4db57ce08be950f4343ce1982a990f357e
KacperMatusiewicz <kacper.piotr.matusiewicz@gmail.com>
2022-03-21 15:56:01 +0100

    FB-25 eureka

--- post/src/main/java/ripoff/facebook/post/PostApplication.java
+++ post/src/main/java/ripoff/facebook/post/PostApplication.java
@@ -2,8 +2,10 @@ package ripoff.facebook.post;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PostApplication {
     public static void main(String[] args) {
         SpringApplication.run(PostApplication.class, args);
--- post/src/main/java/ripoff/facebook/post/PostValidationConfig.java
+++ post/src/main/java/ripoff/facebook/post/PostValidationConfig.java
@@ -1,5 +1,6 @@
 package ripoff.facebook.post;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class PostValidationConfig {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
